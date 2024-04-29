062b4e49b6aaf38ec3f0dd5e531ac94ab82bd954
Emiliano Acevedo <e.acevedo.felip@gmail.com>
2024-04-28 23:02:45 -0400

    Feat: Basic dep check and update

--- bin/up-deps.js
+++ bin/up-deps.js
@@ -1,11 +1,72 @@
 #!/usr/bin/env node
-
+import { createInterface } from 'node:readline/promises'
 import { exec } from 'node:child_process'
 
-async function main () {
-  const { stdout } = await execAsnc('npm list --json')
-  const obj = JSON.parse(stdout)
-  console.log(obj);
+const dependencies = {}
+const devDependencies = {}
+const { stdout: dependenciesJsonString } = await execAsnc('npm list --json --omit=dev')
+const { stdout: allDependenciesJsonString } = await execAsnc('npm list --json')
+for (const packageModule in JSON.parse(dependenciesJsonString).dependencies)
+  dependencies[packageModule] = JSON.parse(dependenciesJsonString).dependencies[packageModule].version
+for (const packageModule in JSON.parse(allDependenciesJsonString).dependencies) {
+  if (dependencies[packageModule]) continue
+  devDependencies[packageModule] = JSON.parse(allDependenciesJsonString).dependencies[packageModule].version
+}
+const positiveAnswers = ['yes', 'y']
+const versionTypes = {
+  MAJOR: { symbol: Symbol('MAJOR'), label: '🔴 Major update' },
+  MINOR: { symbol: Symbol('MINOR'), label: '🟡 Minor update' },
+  FIX: { symbol: Symbol('FIX'), label: '🟢 Fix update' }
+}
+
+await checkVersions(dependencies)
+await checkVersions(devDependencies, true)
+
+/**
+ * @param { Object.<string, string> } dependenciesObject
+ * @param { boolean } isDev
+ */
+async function checkVersions (dependenciesObject, isDev = false) {
+  const depEntries = Object.entries(dependenciesObject)
+  const res = await Promise.all(depEntries.map(checkDepVersion))
+  const oldPackages = res
+    .filter(({ actualVersion, latestVersion }) => actualVersion !== latestVersion)
+  if (!oldPackages.length) return console.log(`✅ ${isDev ? 'devDependencies' : 'dependencies'} are up to date`)
+  for (const { dep, actualVersion, latestVersion } of oldPackages) {
+    console.log(`🔻 ${dep} is out of date: 🔻
+    ${actualizationType({ actualVersion, latestVersion }).label} / ${actualVersion} => ${latestVersion}`)
+  }
+  const updatePachages = await questionUserBoolean('Do you wich to update the packages? (yes/no): ')
+  if (!updatePachages) return
+  await updateDeps(oldPackages, isDev)
+}
+
+/**
+ * @param {{ dep: string; actualVersion: string; latestVersion: string; }[]} deps
+ * @param { boolean } isDev
+ */
+async function updateDeps (deps, isDev = false) {
+  const updateMajors = await questionUserBoolean('Do you wich to update the major versions? (yes/no): ')
+  if (!updateMajors) {
+    deps = deps.filter(({ actualVersion, latestVersion }) =>
+      actualizationType({ actualVersion, latestVersion }).symbol !== versionTypes.MAJOR.symbol)
+  }
+  if (deps.length < 1) return
+  const mappedDeps = deps.map(({ dep }) => `${dep}@latest`).join(' ')
+  let command = `npm install ${mappedDeps} -E`
+  if (isDev) command += ' -D'
+  await execAsnc(command)
+  console.log(`⬆️ Following packages has been updated: ${deps.map(({ dep }) => dep).join(', ')}`)
+}
+
+/**
+ * @param { string[] } param0
+ * @returns { Promise<{dep: string, actualVersion: string, latestVersion: string}> }
+ */
+async function checkDepVersion ([dep, actualVersion]) {
+  const { stdout } = await execAsnc(`npm show ${dep} version`)
+  const latestVersion = stdout.replace(/\n/g, '')
+  return { dep, actualVersion, latestVersion }
 }
 
 /**
@@ -22,4 +83,27 @@ function execAsnc (command) {
   })
 }
 
-main().catch(console.error)
+/**
+ * @param { Object } param0
+ * @param { string } param0.actualVersion
+ * @param { string } param0.latestVersion
+ * @returns { { symbol: symbol, label: string } }
+ */
+function actualizationType ({ actualVersion, latestVersion }) {
+  const [actualMajor, actualMinor, actualFix] = actualVersion.split('.')
+  const [newMajor, newMinor, newFix] = latestVersion.split('.')
+  if (actualMajor !== newMajor) return versionTypes.MAJOR
+  if (actualMinor !== newMinor) return versionTypes.MINOR
+  if (actualFix !== newFix) return versionTypes.FIX
+  return {}
+}
+
+/**
+ * @param { string } question
+ */
+async function questionUserBoolean (question) {
+  const rl = createInterface({ input: process.stdin, output: process.stdout })
+  const answer = await rl.question(question)
+  rl.close()
+  return positiveAnswers.includes(answer)
+}
